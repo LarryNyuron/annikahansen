b1b787a73ef4408ec3a6d485e3ef3b99b32c771b
KirillMoskow <48122830+KirillMoskow@users.noreply.github.com>
2023-01-06 17:09:29 +0300

    UrlPicker maded

--- GazParser.py
+++ GazParser.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 url = "https://www.gazpromnoncoreassets.ru/auctions/purpose/proizvodstvennye-sklady-i-bazy/auctiontype/announces/"
 #url = input('Введите ссылку на сайт: ')
-
 def SiteSaver():   #Функция сохраняет сайт, шоб не забанили
     req = requests.get(url)
     src = req.text
@@ -11,7 +10,7 @@ def SiteSaver():   #Функция сохраняет сайт, шоб не за
         file.write(src)
 
 SiteSaver()
-
+print(url)
 with open('index.html') as file:
     src = file.read()
 
@@ -30,14 +29,30 @@ def ImgParser():    #Функция парсит картинки с сайта
 
 
 gethref = []
-def AnnounceParser(): #Копирует ссылки на продажу
+def AnnounceParser(): #Копирует ссылки на продажу и создает файл с ссылками в ряд
+    f = open('UrlList.txt','w')
     for all_h1 in AI.find_all('div', class_='list-container'):
         for href in all_h1('a'):
            gethref.append('https://www.gazpromnoncoreassets.ru/' + href.get('href'))
-    print(set(gethref))
+    f.write("\n".join(map(str,set(gethref))))
+    f.close()
 
 AnnounceParser()
 
+
+def UrlPicker(): #Отбирает ссылку по запросу для перехода на другой URL
+    global url
+    list = []
+    file = open('UrlList.txt')
+    for i in file:
+        list.append(i)
+    num = list[int(input('номер: '))]
+    url = ""f'{num}'""
+    print(url)
+    file.close()
+
+UrlPicker()
+print(url)
 
 
 
